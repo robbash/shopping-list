e4198ef8fd02026b7b5f4fbd55fcd750f4ac6ff3
Robert Sternberg <rob@robbash.com>
2018-12-29 20:25:26 +1100

    Some code style

--- client/templates/list_items/items_list.js
+++ client/templates/list_items/items_list.js
@@ -10,44 +10,44 @@ Template.itemsList.events({
     e.preventDefault();
 
     var item = {
-      title: $( 'input#new-item' ).val(),
+      title: $('input#new-item').val(),
       checked: false
     };
 
-    Meteor.call( 'itemInsert', item, function( error, result ) {
+    Meteor.call('itemInsert', item, function(error, result) {
       // display the error to the user and abort
       if (error) {
         return alert(error.reason);
       }
 
-      $( 'input#new-item' ).val( '' );
+      $('input#new-item').val('');
     });
   },
   'focus input#new-item': function(e) {
     e.preventDefault();
 
-    var _this = $( 'input#new-item' );
-    var width = _this.parents( '.form-inline' ).innerWidth();
-    
+    var _this = $('input#new-item');
+    var width = _this.parents('.form-inline').innerWidth();
+
     inputWidth = _this.outerWidth();
 
     _this.animate({
         width: width - 95
-    }, 400 );
+    }, 400);
   },
   'blur input#new-item': function(e) {
     e.preventDefault();
 
-    var _this = $( 'input#new-item' );
+    var _this = $('input#new-item');
     _this.animate({
         width: inputWidth
-    }, 400 );
+    }, 400);
   },
   'click #shopping-done': function() {
-    if ( confirm( 'Do you want to remove all checked items from the list?' ) ) {
-      _.each( Items.find( {checked: true} ).fetch(), function( item ) {
-        Items.remove( { _id: item._id } );
-      } )
+    if (confirm('Do you want to remove all checked items from the list?')) {
+      _.each(Items.find({ checked: true }).fetch(), function(item) {
+        Items.remove({ _id: item._id });
+      })
     }
   }
 });
--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ Accounts.config({
    loginExpirationInDays: null
 });
 
-if ( Version.find().count() > 0 ) {
+if (Version.find().count() > 0) {
   Version.remove({});
 }
-Version.insert( JSON.parse( Assets.getText( "version.json" ) ) );
+Version.insert(JSON.parse(Assets.getText('version.json')));
